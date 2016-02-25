2ab3ff20910cf9d74ed304f66e162fc608322163
delsmore <dave@daves-macbook-pro.local>
2016-02-25 07:05:53 +0000

    added templates

--- scat/urls.py
+++ scat/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.service_list, name='service_list'),
+    url(r'^service/(?P<pk>[0-9]+)/$', views.service_detail, name='service_detail'),
 ]
--- scat/views.py
+++ scat/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
 from . models import Service
 from django.utils import timezone
 
@@ -6,3 +6,7 @@ from django.utils import timezone
 def service_list(request):
     services = Service.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'scat/service_list.html', {'services':services})
+    
+def service_detail(request, pk):
+    service = get_object_or_404(Service, pk=pk)
+    return render(request, 'scat/service_detail.html', {'service': service})
