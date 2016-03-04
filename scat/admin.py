2b38016f1891dc0d34314e8909fedd55f19af4c5
David Elsmore <david.elsmore@ed.ac.uk>
2016-03-04 15:20:22 +0000

    db changes

--- scat/admin.py
+++ scat/admin.py
@@ -1,24 +1,26 @@
 from django.contrib import admin
 from .models import Service
-from .models import Person
+from .models import People
 from .models import Category
 from .models import Status
 from .models import Provider
 from .models import Support
 from .models import Type
 from .models import Portfolio
-
+from .models import Location
+from .models import Availability
     
 
 
 admin.site.register(Service)
-admin.site.register(Person)
+admin.site.register(People)
 admin.site.register(Category)
 admin.site.register(Status)
 admin.site.register(Provider)
 admin.site.register(Support)
 admin.site.register(Type)
-admin.site.register(Portfolio)
+admin.site.register(Location)
+admin.site.register(Availability)
 
 
 
--- scat/models.py
+++ scat/models.py
@@ -24,14 +24,12 @@ class Category(models.Model):
     def __str__(self):
         return self.category
     class Meta:
-         ordering = ['category']
-		 
-class CategoryImage(models.Model):
-    category = models.ForeignKey('Category', related_name='images')
-    image = models.ImageField(upload_to='scat/static/images/categories')
+        ordering = ['category']
+        verbose_name_plural = "categories"
+
 		
 class Type(models.Model):
-    type = models.CharField(max_length=100)
+    type = models.CharField(max_length=100,  blank=True, null=True)
     def __str__(self):
         return self.type
 		
@@ -43,7 +41,7 @@ class Provider(models.Model):
     class Meta:
          ordering = ['college','school']
 		
-class Person(models.Model):
+class People(models.Model):
     uun = models.CharField(max_length=8,default=None, blank=True, null=True)
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100,default=None, blank=True, null=True)
@@ -54,38 +52,57 @@ class Person(models.Model):
         return self.name
    
     class Meta:
-         ordering = ['name']
-		 
-
-		
+        ordering = ['name']
+        verbose_name_plural = "people"
+		 	
 class Status(models.Model):
     stage = models.CharField(max_length=50)
     def __str__(self):
         return self.stage
+    class Meta:
+        verbose_name_plural = "status"
         
 class Support(models.Model):
     team = models.CharField(max_length=200)
     def __str__(self):
         return self.team
     class Meta:
-         ordering = ['team']
+        ordering = ['team']
+        verbose_name_plural = "support"
+		 
+class Location(models.Model):
+    location = models.CharField(max_length=200)
+    def __str__(self):
+        return self.location
+    class Meta:
+         ordering = ['location']
+		 
+class Availability(models.Model):
+    hours = models.TextField()
+    def __str__(self):
+        return self.hours
+    class Meta:
+        ordering = ['hours']
+        verbose_name_plural = "availability"
 
 class Service(models.Model):
     title = models.CharField(max_length=200)
     summary = models.CharField(max_length=140, default=None, blank=True, null=True)
     description = models.TextField()
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, default=None, blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None, blank=True, null=True)
-    #type = models.ForeignKey(Type, on_delete=models.CASCADE, default=None, blank=True, null=True)
     type = models.ManyToManyField(Type)
     provider = models.ForeignKey(Provider, on_delete=models.CASCADE, default=None, blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True)
-    business_owner = models.ForeignKey(Person, related_name='bo', on_delete=models.CASCADE, default=None, blank=True, null=True)
-    service_owner = models.ForeignKey(Person, related_name='so', on_delete=models.CASCADE, default=None, blank=True, null=True)
-    service_operations_manager = models.ForeignKey(Person, related_name='som', on_delete=models.CASCADE, default=None, blank=True, null=True)
+    business_owner = models.ForeignKey(People, related_name='bo', on_delete=models.CASCADE, default=None, blank=True, null=True)
+    service_owner = models.ForeignKey(People, related_name='so', on_delete=models.CASCADE, default=None, blank=True, null=True)
+    som = models.ManyToManyField(People)
     status = models.ForeignKey(Status, on_delete=models.CASCADE, default=None, blank=True, null=True)
     support = models.ForeignKey(Support, on_delete=models.CASCADE, default=None, blank=True, null=True)
     documentation = models.CharField(max_length=200,  blank=True, null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True)
+    availability = models.ForeignKey(Availability, on_delete=models.CASCADE, blank=True, null=True)
     logo = models.ImageField(upload_to=only_filename, default=None, blank=True, null=True)
 
     def publish(self):
