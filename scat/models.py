73e21a7ed877a3d18a449292d4d6f93ccfbabae6
David Elsmore <david.elsmore@ed.ac.uk>
2016-03-07 16:34:54 +0000

    null guid

--- scat/models.py
+++ scat/models.py
@@ -87,8 +87,8 @@ class Availability(models.Model):
         verbose_name_plural = "availability"
 
 class Service(models.Model):
-    guid = models.UUIDField(primary_key=False, default=None, editable=True)
     title = models.CharField(max_length=200)
+    guid = models.UUIDField(primary_key=False, default=None, null=True, blank=True, editable=True)
     summary = models.CharField(max_length=140, default=None, blank=True, null=True)
     description = models.TextField()
     portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, default=None, blank=True, null=True)
